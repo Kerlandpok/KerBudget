031b753ddecd6f64dcf66b2585165048dec235b4
Kerlandpok <93793807+Kerlandpok@users.noreply.github.com>
2026-07-27 23:10:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kerbudget-v6-20260727';
+const CACHE='kerbudget-v7-20260727';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./data.js','./excel-extra.js','./manifest.json','./icon.svg'];
 
 self.addEventListener('install', event=>{
